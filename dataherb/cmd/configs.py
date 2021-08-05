17fc4677451b25826d89413ec6903615218e4f0e
LM <hi@leima.is>
2021-08-06 01:10:19 +0200

    added configuration tool

--- dataherb/cmd/configs.py
+++ dataherb/cmd/configs.py
@@ -13,16 +13,29 @@ def load_dataherb_config(config_path=None):
         home = Path.home()
         config_path = home / ".dataherb/config.json"
 
-    logger.debug(f"Using {config_path} as config file for dataherb")
-    with open(config_path, "r") as f:
-        conf = json.load(f)
-
-    if not conf.get("workdir"):
+    if not config_path.exists():
         logger.error(
-            f"Please specify working directory in the config file using the key workdir"
+            f"Config file {config_path} does not exist.\n"
+            f"If this is the first time you use dataherb, please run `dataherb configure` to config dataherb."
         )
-    elif conf.get("workdir", "").startswith("~"):
-        home = Path.home()
-        conf["workdir"] = str(home / conf["workdir"][2:])
+
+    logger.debug(f"Using {config_path} as config file for dataherb")
+    try:
+        with open(config_path, "r") as f:
+            conf = json.load(f)
+
+        if not conf.get("workdir"):
+            logger.error(
+                f"Please specify working directory in the config file using the key workdir"
+            )
+        elif conf.get("workdir", "").startswith("~"):
+            home = Path.home()
+            conf["workdir"] = str(home / conf["workdir"][2:])
+    except json.decoder.JSONDecodeError:
+        logger.error(
+            f"Config file {config_path} is not valid json.\n"
+            f"Please rerun `dataherb configure` to reconfi dataherb or manually fix it."
+        )
+        conf = {}
 
     return conf
