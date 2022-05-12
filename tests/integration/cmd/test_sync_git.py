02e6f967f304dc79c84fe647d069450843b8de97
LM <hi@leima.is>
2022-05-12 22:01:48 +0200

    update sync_git

---
+++ tests/integration/cmd/test_sync_git.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from dataherb.cmd.sync_git import is_git_repo
+import pytest
+
+
+@pytest.mark.parametrize("path, expected", [pytest.param(Path("."), False)])
+def test_is_git_repo(path, expected):
+
+    assert is_git_repo(path), expected
