a984ce6a194f1a2b0aea3691380f97aa8ea64679
LM <hi@leima.is>
2022-05-12 22:03:08 +0200

    update sync_s3

--- dataherb/cmd/sync_s3.py
+++ dataherb/cmd/sync_s3.py
@@ -1,9 +1,12 @@
 from dataherb.utils.awscli import aws_cli as _aws_cli
 
 
-def upload_dataset_to_s3(source, target):
+def upload_dataset_to_s3(source: str, target: str) -> None:
     """
     upload_dataset_to_s3 uploads the dataset to S3
+
+    :param source: local folder
+    :param target: remote s3 uri
     """
 
     _aws_cli(("s3", "sync", source, target))
